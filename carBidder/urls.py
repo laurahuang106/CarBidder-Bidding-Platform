ed6887c216546cc86c6887fab64eba90af12f48e
yingguocs22 <guoyingcs@163.com>
2023-11-13 15:27:15 -0800

    add register

--- carBidder/urls.py
+++ carBidder/urls.py
@@ -20,4 +20,5 @@ from carBidderApp import views
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path('', views.testmysql),
+    path('login/', views.register),
 ]
--- carBidderApp/views.py
+++ carBidderApp/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.db import connection
-# from .models import Employee
+from django.contrib.auth.hashers import make_password
+import uuid 
 
-# Create your views here.
 def testmysql(request):
     with connection.cursor() as cursor:
         cursor.execute("""
@@ -16,3 +16,28 @@ def testmysql(request):
         'user_name': rows,
     }
     return render(request, 'home.html', context)
+
+def register(request):
+    if request.method == "POST":
+        try:
+            # Get data from POST request
+            user_type = request.POST.get('user_type', '')
+            user_name = request.POST.get('user_name', '')
+            email = request.POST.get('email', '')
+
+            # Insert data into the database
+            with connection.cursor() as cursor:
+                query = """
+                    INSERT INTO USERS (user_type, user_name, email)
+                    VALUES (%s, %s, %s);
+                """
+                cursor.execute(query, (user_type, user_name, email))
+                connection.commit()
+        except Exception as e:
+            # Handle any errors that occur during the process
+            print(f"An error occurred: {e}")
+            # Optionally, add error handling logic here (e.g., set an error message, rollback transaction)
+
+    # Render the registration form for both GET and POST requests
+    return render(request, 'register.html')
+
