5b038da02b50e512e8bb2170a6252419f1d4479d
yingguocs22 <guoyingcs@163.com>
2023-11-21 13:53:28 -0800

    merge from Yuhan

--- carBidderApp/views.py
+++ carBidderApp/views.py
@@ -7,14 +7,14 @@ cur_user = {}
 
 def home(request):
 
-    # user_type = request.session.get('user_type', '')
-    # user_name = request.session.get('user_name', '')
-    cur_user = request.session.get('cur_user', '')
-    # context = {
-    #     'user_type': user_type,
-    #     'user_name': user_name,
-    # }
-    return render(request, 'home.html', cur_user)
+    user_type = request.session.get('user_type', '')
+    user_name = request.session.get('user_name', '')
+    # cur_user = request.session.get('cur_user', '')
+    context = {
+        'user_type': user_type,
+        'user_name': user_name,
+    }
+    return render(request, 'home.html', context)
 
 def register(request):
     if request.method == "POST":
@@ -63,38 +63,13 @@ def login(request):
                     cur_user = t
                     print(cur_user)
                     user_data = t[0]
-                    user_id = user_data[0]
                     user_type = user_data[1]
                     user_name = user_data[2]
-                    email = user_data[3]
-<<<<<<< HEAD
-
-=======
-                    balance = user_data[4] # Decimal
-                    seller_rating = user_data[5] # Decimal
-                    buyer_rating = user_data[6] # Decimal
-                    num_of_seller_rating = user_data[7]
-                    num_of_buyer_rating = user_data[8]
-                    is_allow_chat = user_data[9]
-                    is_allow_list = user_data[10]
-                    
-                    # Constructing cur_user dictionary with all user details
->>>>>>> 80938aae30202d44130f74fef04963dd0c20c88b
-                    cur_user = {
-                        'user_id': user_id,
+                    context = {
                         'user_type': user_type,
                         'user_name': user_name,
-                        'email': email,
-                        'balance': str(balance),  # Converting Decimal to string for display
-                        'seller_rating': str(seller_rating),
-                        'buyer_rating': str(buyer_rating),
-                        'num_of_seller_rating': num_of_seller_rating,
-                        'num_of_buyer_rating': num_of_buyer_rating,
-                        'is_allow_chat': is_allow_chat,
-                        'is_allow_list': is_allow_list,
                     }
-                    request.session['cur_user'] = cur_user
-
+                    request.session['context'] = context
                     return redirect('home')
         except Exception as e:
             # Handle any errors that occur during the process
@@ -111,9 +86,4 @@ def logout(request):
 
 def profile(request):
 
-    # context = {
-    #     'user_type': "user_type",
-    #     'user_name': "user_name",
-    # }
-    cur_user = request.session.get('cur_user', '')
     return render(request, 'profile.html', cur_user)
