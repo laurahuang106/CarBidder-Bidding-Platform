2de2eec5fc24125e4be852dcb4c5a894f913ab19
CarolineFang1998 <f767475251@gmail.com>
2023-11-21 13:23:34 -0800

    try to get all the values

--- carBidder/urls.py
+++ carBidder/urls.py
@@ -19,7 +19,7 @@ from carBidderApp import views
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('', views.testmysql, name='home'),
+    path('', views.home, name='home'),
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
--- carBidderApp/views.py
+++ carBidderApp/views.py
@@ -5,7 +5,7 @@ import uuid
 
 cur_user = {}
 
-def testmysql(request):
+def home(request):
     # with connection.cursor() as cursor:
     #     cursor.execute("""
     #         select VIN
@@ -70,10 +70,31 @@ def login(request):
                     cur_user = t
                     print(cur_user)
                     user_data = t[0]
+                    user_id = user_data[0]
                     user_type = user_data[1]
                     user_name = user_data[2]
-                    request.session['user_type'] = user_type
-                    request.session['user_name'] = user_name
+                    email = user_data[3]
+                    balance = user_data[4]
+                    seller_rating = user_data[5]
+                    buyer_rating = user_data[6]
+                    num_of_seller_rating = user_data[7]
+                    num_of_buyer_rating = user_data[8]
+                    is_allow_chat = user_data[9]
+                    is_allow_list = user_data[10]
+
+                    # # Assigning values to the session 
+                    # request.session['user_id'] = user_id
+                    # request.session['user_type'] = user_type
+                    # request.session['user_name'] = user_name
+                    # request.session['email'] = email
+                    # request.session['balance'] = balance
+                    # request.session['seller_rating'] = seller_rating
+                    # request.session['buyer_rating'] = buyer_rating
+                    # request.session['num_of_seller_rating'] = num_of_seller_rating
+                    # request.session['num_of_buyer_rating'] = num_of_buyer_rating
+                    # request.session['is_allow_chat'] = is_allow_chat
+                    # request.session['is_allow_list'] = is_allow_list
+
                     return redirect('home')
         except Exception as e:
             # Handle any errors that occur during the process
@@ -89,5 +110,29 @@ def logout(request):
     return redirect('home')
 
 def profile(request):
+    user_id = request.session.get('user_id', '')
+    user_type = request.session.get('user_type', '')
+    user_name = request.session.get('user_name', '')
+    email = request.session.get('email', '')
+    balance = request.session.get('balance', '')
+    seller_rating = request.session.get('seller_rating', '')
+    buyer_rating = request.session.get('buyer_rating', '')
+    num_of_seller_rating = request.session.get('num_of_seller_rating', '')
+    num_of_buyer_rating = request.session.get('num_of_buyer_rating', '')
+    is_allow_chat = request.session.get('is_allow_chat', '')
+    is_allow_list = request.session.get('is_allow_list', '')
 
-    return render(request, 'profile.html')
+    context = {
+        'user_id': user_id,
+        'user_type': user_type,
+        'user_name': user_name,
+        'email': email,
+        'balance': balance,
+        'seller_rating': seller_rating,
+        'buyer_rating': buyer_rating,
+        'num_of_seller_rating': num_of_seller_rating,
+        'num_of_buyer_rating': num_of_buyer_rating,
+        'is_allow_chat': is_allow_chat,
+        'is_allow_list': is_allow_list,
+    }
+    return render(request, 'profile.html', context)
