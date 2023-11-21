06eb805103d1635488ba729e431bef79f28e45f2
yingguocs22 <guoyingcs@163.com>
2023-11-21 10:20:46 -0800

    add extends

--- carBidderApp/models.py
+++ carBidderApp/models.py
@@ -5,22 +5,22 @@
 #   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
-from django.db import models
+# from django.db import models
 
 
-class Users(models.Model):
-    user_id = models.IntegerField(primary_key=True)
-    user_type = models.CharField(max_length=11)
-    user_name = models.CharField(max_length=30)
-    email = models.CharField(unique=True, max_length=255, blank=True, null=True)
-    balance = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
-    seller_rating = models.DecimalField(max_digits=3, decimal_places=2, blank=True, null=True)
-    buyer_rating = models.DecimalField(max_digits=3, decimal_places=2, blank=True, null=True)
-    num_of_seller_rating = models.IntegerField(blank=True, null=True)
-    num_of_buyer_rating = models.IntegerField(blank=True, null=True)
-    is_allowed_chat = models.IntegerField(blank=True, null=True)
-    is_allow_list = models.IntegerField(blank=True, null=True)
+# class Users(models.Model):
+#     user_id = models.IntegerField(primary_key=True)
+#     user_type = models.CharField(max_length=11)
+#     user_name = models.CharField(max_length=30)
+#     email = models.CharField(unique=True, max_length=255, blank=True, null=True)
+#     balance = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+#     seller_rating = models.DecimalField(max_digits=3, decimal_places=2, blank=True, null=True)
+#     buyer_rating = models.DecimalField(max_digits=3, decimal_places=2, blank=True, null=True)
+#     num_of_seller_rating = models.IntegerField(blank=True, null=True)
+#     num_of_buyer_rating = models.IntegerField(blank=True, null=True)
+#     is_allowed_chat = models.IntegerField(blank=True, null=True)
+#     is_allow_list = models.IntegerField(blank=True, null=True)
 
-    class Meta:
-        # managed = False
-        db_table = 'USERS'
+#     class Meta:
+#         # managed = False
+#         db_table = 'USERS'
